73e3838482d2f73c1e2bdda3ac9f771ea46f667f
Ian O'Rourke <cessna.ian@gmail.com>
2025-12-20 16:42:00 -0600

    Cleanup rig logic

--- src/rigctl.rs
+++ src/rigctl.rs
@@ -4,21 +4,6 @@ use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 use std::sync::{Arc, Mutex};
 
-enum CommandOptions {
-    GetFrequency,
-    SetFrequency(u64),
-    GetVFO,
-    GetMode,
-    SetMode,
-    SetTransmit(bool),
-    SetVfoOpt(bool),
-    Boolean(bool),
-    DumpState,
-    Exit,
-    Success,
-    Error,
-}
-
 fn handle_rigctl_client(
     mut stream: TcpStream,
     _ser: Arc<Mutex<Box<dyn serialport::SerialPort + Send>>>,
@@ -61,30 +46,35 @@ fn handle_rigctl_client(
 
         debug_file.as_mut().map(|f| writeln!(f, "{}", cmd).unwrap());
 
-        fn get_frequency_command<T: AsRef<str>>(s: Option<T>) -> CommandOptions {
+        fn get_frequency_command<T: AsRef<str>>(s: Option<T>) -> Option<u64> {
             if let Some(val) = s {
-                let parsed_hz = if let Ok(hz_int) = val.as_ref().parse::<u64>() {
+                if let Ok(hz_int) = val.as_ref().parse::<u64>() {
                     Some(hz_int)
                 } else if let Ok(hz_f) = val.as_ref().parse::<f64>() {
                     Some(hz_f.round() as u64)
                 } else {
                     None
-                };
-
-                // Check if frequency parsed successfully
-                if let Some(hz) = parsed_hz {
-                    CommandOptions::SetFrequency(hz)
-                } else {
-                    CommandOptions::Error
                 }
             } else {
-                CommandOptions::Error
+                None
             }
         }
 
         fn get_boolean_from_num<T: AsRef<str>>(s: Option<T>) -> bool {
             s.map(|v| v.as_ref().parse::<i32>().map(|x| x != 0).unwrap_or(false))
                 .unwrap_or(false)
+        }
+
+        fn write_success(stream: &mut TcpStream) {
+            let _ = writeln!(stream, "RPRT 0");
+        }
+
+        fn write_failure(stream: &mut TcpStream) {
+            let _ = writeln!(stream, "RPRT -1");
+        }
+
+        fn write_boolean(stream: &mut TcpStream, val: bool) {
+            let _ = writeln!(stream, "{}", if val { 1 } else { 0 });
         }
 
         let cmds = cmd.split_ascii_whitespace().collect::<Vec<_>>();
@@ -97,105 +87,14 @@ fn handle_rigctl_client(
                 .map(|f| writeln!(f, "    {i} => {c}").unwrap());
         }
 
-        let command_response = match cmds[0] {
-            "\\set_vfo_opt" => CommandOptions::SetVfoOpt(get_boolean_from_num(cmds.get(1))),
-            "\\chk_vfo" => CommandOptions::Boolean(vfo_mode),
-            "\\get_powerstat" => CommandOptions::Boolean(true),
-            "\\dump_state" => CommandOptions::DumpState,
-            "\\get_freq" | "f" => {
-                if (cmds.len() == 1 && !vfo_mode)
-                    || (cmds.len() == 2 && vfo_mode && cmds[1] == current_vfo)
-                {
-                    CommandOptions::GetFrequency
-                } else {
-                    CommandOptions::Error
-                }
+        match cmds[0] {
+            "\\set_vfo_opt" => {
+                vfo_mode = get_boolean_from_num(cmds.get(1));
+                write_success(&mut stream);
             }
-            "\\set_freq" | "F" => {
-                if cmds.len() == 2 && !vfo_mode {
-                    get_frequency_command(cmds.get(1))
-                } else if cmds.len() == 3 && vfo_mode && cmds[1] == current_vfo {
-                    get_frequency_command(cmds.get(2))
-                } else {
-                    CommandOptions::Error
-                }
-            }
-            "\\dump_caps" => CommandOptions::Success,
-            "m" => CommandOptions::GetMode,
-            "M" => CommandOptions::SetMode,
-            "v" => CommandOptions::GetVFO,
-            "V" => CommandOptions::Success,
-            "t" | "\\get_ptt" => CommandOptions::Boolean(*tx_state.lock().unwrap()),
-            "T" | "\\set_ptt" => {
-                // Check if command has TX state parameter
-                if cmds.len() >= 2 {
-                    CommandOptions::SetTransmit(get_boolean_from_num(cmds.get(1)))
-                } else {
-                    CommandOptions::Error
-                }
-            }
-            "q" => CommandOptions::Exit,
-            _ => CommandOptions::Error,
-        };
-
-        match command_response {
-            CommandOptions::Boolean(val) => {
-                let _ = writeln!(stream, "{}", if val { "1" } else { "0" });
-            }
-            CommandOptions::GetFrequency => {
-                let hz = *freq_state.lock().unwrap();
-                let _ = writeln!(stream, "{}", hz);
-            }
-            CommandOptions::SetVfoOpt(val) => {
-                vfo_mode = val;
-                let _ = writeln!(stream, "RPRT 0");
-            }
-            CommandOptions::SetFrequency(hz) => {
-                *freq_state.lock().unwrap() = hz;
-                {
-                    let mut q = cat_queue.lock().unwrap();
-                    q.push(format!("FA{:011};", hz).into_bytes());
-                }
-                let _ = writeln!(stream, "RPRT 0");
-            }
-            CommandOptions::GetVFO => {
-                let _ = writeln!(stream, "{current_vfo}");
-            }
-            CommandOptions::GetMode => {
-                let _ = writeln!(stream, "USB");
-                let _ = writeln!(stream, "2400");
-            }
-            CommandOptions::SetMode => {
-                {
-                    let mut q = cat_queue.lock().unwrap();
-                    q.push(b"MD2;".to_vec());
-                }
-                let _ = writeln!(stream, "RPRT 0");
-            }
-            CommandOptions::SetTransmit(val) => {
-                // Check if serial port lock acquired successfully
-                if let Ok(mut s) = _ser.lock() {
-                    // Check if TX should be enabled
-                    if val {
-                        let _ = trusdx::start_transmit_baseband(&mut **s);
-                    } else {
-                        let _ = trusdx::enable_streaming_speaker_off(&mut **s);
-                    }
-                }
-                *tx_state.lock().unwrap() = val;
-                let _ = writeln!(stream, "RPRT 0");
-            }
-            CommandOptions::Exit => {
-                let _ = writeln!(stream, "RPRT 0");
-                break;
-            }
-            CommandOptions::Success => {
-                let _ = writeln!(stream, "RPRT 0");
-            }
-            CommandOptions::Error => {
-                let _ = writeln!(stream, "RPRT -1");
-            }
-            CommandOptions::DumpState => {
+            "\\chk_vfo" => write_boolean(&mut stream, vfo_mode),
+            "\\get_powerstat" => write_boolean(&mut stream, true),
+            "\\dump_state" => {
                 let lines = [
                     "0",
                     "0",
@@ -221,7 +120,83 @@ fn handle_rigctl_client(
                     let _ = writeln!(stream, "{}", l);
                 }
             }
-        }
+            "\\get_freq" | "f" => {
+                if (cmds.len() == 1 && !vfo_mode)
+                    || (cmds.len() == 2 && vfo_mode && cmds[1] == current_vfo)
+                {
+                    let hz = *freq_state.lock().unwrap();
+                    let _ = writeln!(stream, "{}", hz);
+                } else {
+                    write_failure(&mut stream);
+                }
+            }
+            "\\set_freq" | "F" => {
+                let parsed_hz = if cmds.len() == 2 && !vfo_mode {
+                    get_frequency_command(cmds.get(1))
+                } else if cmds.len() == 3 && vfo_mode && cmds[1] == current_vfo {
+                    get_frequency_command(cmds.get(2))
+                } else {
+                    write_failure(&mut stream);
+                    continue;
+                };
+
+                if let Some(hz) = parsed_hz {
+                    *freq_state.lock().unwrap() = hz;
+                    {
+                        let mut q = cat_queue.lock().unwrap();
+                        q.push(format!("FA{:011};", hz).into_bytes());
+                    }
+                    write_success(&mut stream);
+                }
+            }
+            "\\dump_caps" => {
+                write_success(&mut stream);
+            }
+            "m" => {
+                let _ = writeln!(stream, "USB");
+                let _ = writeln!(stream, "2400");
+            }
+            "M" => {
+                {
+                    let mut q = cat_queue.lock().unwrap();
+                    q.push(b"MD2;".to_vec());
+                }
+                write_success(&mut stream);
+            }
+            "v" => {
+                let _ = writeln!(stream, "{current_vfo}");
+            }
+            "V" => {
+                write_success(&mut stream);
+            }
+            "t" | "\\get_ptt" => write_boolean(&mut stream, *tx_state.lock().unwrap()),
+            "T" | "\\set_ptt" => {
+                // Check if command has TX state parameter
+                if cmds.len() >= 2 {
+                    let val = get_boolean_from_num(cmds.get(1));
+                    // Check if serial port lock acquired successfully
+                    if let Ok(mut s) = _ser.lock() {
+                        // Check if TX should be enabled
+                        if val {
+                            let _ = trusdx::start_transmit_baseband(&mut **s);
+                        } else {
+                            let _ = trusdx::enable_streaming_speaker_off(&mut **s);
+                        }
+                    }
+                    *tx_state.lock().unwrap() = val;
+                    write_success(&mut stream);
+                } else {
+                    write_failure(&mut stream);
+                }
+            }
+            "q" => {
+                write_success(&mut stream);
+                break;
+            }
+            _ => {
+                write_failure(&mut stream);
+            }
+        };
 
         debug_file.as_mut().map(|f| f.flush().unwrap());
     }
